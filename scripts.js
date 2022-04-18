55033441224476f2581d6e8d9d5b6b93d1e78625
Brin-o <49071768+Brin-o@users.noreply.github.com>
2022-04-18 15:02:59 +0200

    added helper function for getting variables, added ignore

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 //TODO: dodaj tracking za to v kateri sobi je igralec
+
+function getBipsiVar(varname) {
+    const bipsi = document.getElementById("bipsi").contentWindow
+    console.log(bipsi)
+
+    console.log("Fetching variable of name: ", varname)
+
+    var r = bipsi.PLAYBACK.variables.get(varname)
+    console.log("VALUE OF ", varname, " IS: ", r)
+    return r
+}
