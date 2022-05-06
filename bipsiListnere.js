10c692be27abad22f3b55f6e4426a2d42f725990
Brin-o <49071768+Brin-o@users.noreply.github.com>
2022-05-06 14:14:50 +0200

    new bipsi, key up events

---
+++ bipsiListnere.js
@@ -0,0 +1,13 @@
+document.addEventListener('keyup', (event) => {
+    var name = event.key;
+    var code = event.code;
+    console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+
+    // if (code = "Space") {
+    //     parent.startVoiceMsg()
+    // }
+
+
+}, false);
+
+
